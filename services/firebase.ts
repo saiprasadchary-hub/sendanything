2126cfb2005c5bbc03d9eafe739c80c169a10e56
Sai Prasad Chary Kammari <kspchary077@gmail.com>
2026-01-11 20:37:54 +0530

    Secure API keys and update configuration

--- services/firebase.ts
+++ services/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhyo8ZDJgrYOMiIlmBwfWrXaNH2ovncOA",
-    authDomain: "send-anything.firebaseapp.com",
-    projectId: "send-anything",
-    storageBucket: "send-anything.firebasestorage.app",
-    messagingSenderId: "972402855262",
-    appId: "1:972402855262:web:33d9a2f32a672b424313cf",
-    measurementId: "G-XBLXZDRF0M"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
